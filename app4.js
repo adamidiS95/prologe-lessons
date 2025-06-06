84cbce67a3e22220914c6296db94a216e967a5a2
adamidiS95 <el_barto_s@mail.ru>
2025-06-06 12:31:09 +0300

    Update app4.js

--- app4.js
+++ app4.js
@@ -1,7 +1,7 @@
 const money = 100;
 const canBuy = money > 50;
 
-if (money > 50) {
+if (money > 60) {
     console.log('Может купить')
 } else if (money > 55){
     console.log ('Куплен мини продукт')
